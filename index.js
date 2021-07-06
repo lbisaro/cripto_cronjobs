46063e9a78511edfc825bf336841c0c26b4b94ff
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2021-07-06 16:16:53 -0300

    ajuste en el cron con argumentos

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const cron = require('node-cron');
 const { db } = require("./util");
 const MainCtrl = require("./controllers/MainCtrl").MainCtrl;
 
-//MainCtrl.getPrices();
-//process.exit();
+var args = process.argv.slice(2);
 
-/*
-  Crontab cada 1 minuto, en el segundo numero 55 
-  (Al final de cada minuto para tener casi el ultimo precio)
-  */
-cron.schedule("55 * * * * *", () => {
-  let dt = new Date;
-  console.log(dt.toString());
+if (args[0] == '--nocron'){
   MainCtrl.getPrices();
-});
+  process.exit();
+}
+else{
+  /*
+    Crontab cada 1 minuto, en el segundo numero 55 
+    (Al final de cada minuto para tener casi el ultimo precio)
+    */
+    cron.schedule("55 * * * * *", () => {
+      let dt = new Date;
+      console.log(dt.toString());
+      MainCtrl.getPrices();
+    });
+}
+
+
